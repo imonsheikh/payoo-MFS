bef1a88c77b40c97cc20cd2d5048111e6b877209
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-21 21:16:41 +0600

    add event handler for add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
-console.log('home added');
+// console.log('home added');
+
+/**
+ * step-1: add event handler
+ * prevent page reload after form submit
+ * step-2: get money to be added to the account balance
+ * step-3: get the pin Number provided 
+ */
+
+// add money to the account 
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    // console.log("money add button clicked");
+    // prevent page reload after form submit 
+event.preventDefault()
+
+// step-2: get money to be added to the account balance
+const addMoneyInput = document.getElementById('input-add-money').value
+    console.log(addMoneyInput);
+
+    // step-3: get the pin Number provided 
+    const pinNumberInput = docu
+    
+})
