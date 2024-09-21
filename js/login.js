b7da97f49627c0ff45e4d1353e9be7fd545cb01e
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-21 20:43:03 +0600

    created add money and cash out and others

--- js/login.js
+++ js/login.js
@@ -7,9 +7,20 @@ document.getElementById('button-login').addEventListener('click', function(e){
     event.preventDefault() // Vejal to begginners
    console.log('login button clicked');
 
-//    step-3: get the phone number 
+//    step-3: get the phone number and the phone number
    const phoneNumber = document.getElementById('phone-number').value
-   console.log(phoneNumber);
+   const pinNumber = document.getElementById('pin-number').value
+   console.log(phoneNumber, pinNumber);
+
+    // step-4: Validate phone and Pin
+   //This is temporary. you should not do like this
+   if(phoneNumber === '5' && pinNumber === '1234'){
+    console.log('you are loggin in'); 
+    // step-5: allow user to use the website
+    window.location.href = '/home.html'
+   }else{
+    alert('wrong phone number or PIN')
+   }
 
    
    
